1031f638f972d4f389681a4c6531f6940d1f94c4
Frobi <frobi.webdev@outlook.com>
2025-10-27 18:03:43 +1030

    create provider for cart operations

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { ProductProvider } from './features/product/ProductContext.jsx'
-import './index.css'
-import router from './routes/router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ProductProvider } from "./features/product/ProductContext.jsx";
+import { CartProvider } from "./features/cart/CartContext.jsx";
+import "./index.css";
+import router from "./routes/router.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ProductProvider>
-      <RouterProvider router={router} />
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
     </ProductProvider>
   </StrictMode>,
-)
+);
