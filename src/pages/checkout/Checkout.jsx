cbc830447d924583e8f813021233ec70ec880588
Frobi <frobi.webdev@outlook.com>
2026-09-07 17:40:17 +0930

    Add checkout list with totals to checkout page

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,8 +1,68 @@
+import { useEffect } from "react";
+import styles from "./Checkout.module.css";
+import { useCart } from "../../features/cart/CartContext.jsx";
+
+function CheckoutItem( { item } ) {
+  const product = item.product;
+
+  return <div className={styles.checkoutItem}>
+    <img
+      className={styles.productImage}
+      src={product.images[0]}
+      alt={`${product.brand} ${product.model}`}
+    />
+    <div className = {styles.productInfo}>
+      <p className={styles.productName}>{product.brand} {product.model}</p>
+      <div className={styles.productCalculation}>
+        <p>${product.price} × {item.qtyInCart}</p>
+        <p>${(product.price * item.qtyInCart).toFixed(2)}</p>
+      </div>
+    </div>
+  </div>;
+}
+
+function CheckoutList() {
+  const { cartItems } = useCart();
+
+  if (cartItems.length === 0) {
+    return <p className={styles.emptyMessage}>Your cart is empty.</p>;
+  }
+
+  const total = cartItems.reduce(
+    (sum, item) => sum + item.product.price * item.qtyInCart,
+    0
+  );
+
+  return(
+    <>
+      <div className={styles.checkoutList}>
+        {cartItems.map((item =>
+          <div key={item.product.id}>
+            <CheckoutItem item={item}/>
+          </div>
+        ))}
+      </div>
+      <div className={styles.checkoutTotal}>
+        <p>Total:</p>
+        <p>${total.toFixed(2)}</p>
+      </div>
+    </>
+  );
+}
+
 function Checkout() {
+  useEffect(() => {
+    document.title = "Checkout | RTZ Garage";
+
+    return () => {
+      document.title = "RTZ Garage";
+    };
+  });
 
   return (
     <>
-      <h1>Checkout</h1>
+      <h1 className={styles.checkoutHeading}>Checkout</h1>
+      <CheckoutList/>
     </>
   );
 }
