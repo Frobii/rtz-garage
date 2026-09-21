647649bbdbd0247a6030793cab85417e1d6190e3
Frobi <frobi.webdev@outlook.com>
2026-09-21 15:26:31 +0930

    remove about page link

--- src/pages/media/Media.jsx
+++ src/pages/media/Media.jsx
@@ -1,8 +1,14 @@
+import YouTube from "../../features/socials/YouTube";
+
 function Media() {
 
   return (
     <>
       <h1>Media</h1>
+      <YouTube
+        videoId="O0cpb7diGWo&t=515s"
+        title="350Z"
+      />
     </>
   );
 }
